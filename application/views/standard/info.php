5b8fe3a5f4e0471952d988545ddf0b53d869ee33
Aniket Pant <me@aniketpant.com>
2013-05-20 19:48:45 +0530

    Update info page
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- application/views/standard/info.php
+++ application/views/standard/info.php
@@ -3,7 +3,6 @@
 <section>
   <div class="page-header"><h1>Information <small>How to use?</small></h1></div>
   <ul>
-    <li>Search using ID No. or initials of the name.<br/>e.g. To search for Rahul Chaudhary search either by 2010XXPSXXXG or Rah or Chau</li>
     <li>You will not be able to find another person unless he/she has updated the profile.</li>
     <li>Profile picture dimensions are restricted. It is clearly mentioned on the edit page too.</li>
   </ul>
